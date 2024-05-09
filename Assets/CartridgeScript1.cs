601ed638da0b3accf651fbf43aae7ec5dfa1e4a3
oncelerr <markjonathan368@gmail.com>
2024-05-10 03:01:41 +0800

    Revert "2nd push"
    This reverts commit 9b1807f2fa4e1e024d0b8018640af464b68f60a3.

--- Assets/CartridgeScript1.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class CartridgeScript1 : MonoBehaviour
-{
-    public string sceneName; // Name of the scene to load
-
-    // Additional functionalities for the cartridge can be added here if needed
-}
